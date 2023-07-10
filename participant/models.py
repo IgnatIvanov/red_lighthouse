4832592c880077458758a4b209c897cca0c51b93
Ignat Ivanov <i.ivanov@callforce.pro>
2023-07-11 00:04:39 +0300

    ref(participant): model changed

--- participant/models.py
+++ participant/models.py
@@ -6,4 +6,5 @@ class Participant(models.Model):
 
     dog_id = models.IntegerField('id собаки')
     event_id = models.IntegerField('id события')
+    class_id = models.IntegerField('id класса')
     is_pay = models.BooleanField('Признак оплаты')
