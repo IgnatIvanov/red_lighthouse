dcbe77e53df57e684e2a137ff375f660ee6b9ebb
Ignat Ivanov <i.ivanov@callforce.pro>
2023-07-10 21:50:59 +0300

    ref(type, rank): event model changed: type and rank of events added

--- event/admin.py
+++ event/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Event
+from .models import Event, Type, Rank
 
 
 admin.site.register(Event)
+admin.site.register(Type)
+admin.site.register(Rank)
--- event/models.py
+++ event/models.py
@@ -16,3 +16,30 @@ class Event(models.Model):
 
     # def __str__(self):
     #     return "{}. {} - {}".format(self.id, self.name_ru, self.name_en)
+
+
+class Type(models.Model):
+
+    name = models.TextField('Тип события')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        
+        verbose_name = 'Тип'
+        verbose_name_plural = 'Типы'
+
+
+class Rank(models.Model):
+
+    name = models.TextField('Ранг события')    
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        
+        verbose_name = 'Ранг'
+        verbose_name_plural = 'Ранги'
+
